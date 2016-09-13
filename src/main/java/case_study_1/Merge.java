138a46b12dc328cc9d395a436c8bc3f92941c2ee
AnnaChepiga <anna.chepiga@list.ru>
2016-09-13 15:26:27 +0300

    Merge edited

--- src/main/java/case_study_1/Merge.java
+++ src/main/java/case_study_1/Merge.java
@@ -1,12 +1,27 @@
 package case_study_1;
 
 public class Merge {
-    public static void main(String[] args) {
-        char[] array = {'1', '2', '3', '4', '5'};
+   public static void main(String[] args) {
+      char[] array = {'1', '2', 'a', '4', '5'};
 
-        for (char element : array) {
-            int i = Character.getNumericValue(element);
-            System.out.print(i);
-        }
-    }
+      try {
+         int result = getNumber(array);
+         System.out.println(result);
+      } catch (NumberFormatException e) {
+         System.out.println(e.getMessage());
+      }
+   }
+
+   private static int getNumber(char[] chars) {
+      StringBuilder outputNumber = new StringBuilder();
+
+      for (char element : chars) {
+         if (!Character.isDigit(element)) {
+            throw new NumberFormatException("not numbers");
+         }
+         outputNumber.append(element);
+      }
+
+      return Integer.parseInt(outputNumber.toString());
+   }
 }
