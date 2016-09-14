236f1cf798742680b45ff3ac26f35110fe6f5c53
AnnaChepiga <anna.chepiga@list.ru>
2016-09-14 10:31:52 +0300

    Matrix Snake edited

--- src/main/java/case_study_1/MatrixSnake.java
+++ src/main/java/case_study_1/MatrixSnake.java
@@ -3,38 +3,36 @@ package case_study_1;
 import java.util.Arrays;
 
 public class MatrixSnake {
-    public static void main(String[] args) {
-        int[][] matrix = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 16}
-        };
+   public static void main(String[] args) {
+      int[][] matrix = {
+              {1, 2, 3, 4},
+              {5, 6, 7, 8},
+              {9, 10, 11, 12},
+              {13, 14, 15, 16}
+      };
 
-        int size = matrix.length; // for square matrix
-        int x, y = 0; // coordinates
+      int size = matrix.length; // for square matrix
+      int x, y = 0; // coordinates
 
-        int[] array = new int[size * size];
-        int k = 0; //index in array
+      int[] array = new int[size * size];
+      int k = 0; //index in array
 
-        try {
-            for (; y < size; y++) {
-                for (x = 0; x < size; x++) {
-                    array[k] = matrix[x][y];
-                    k++;
-                }
+      for (; y < size; y++) {
+         for (x = 0; x < size; x++) {
+            array[k] = matrix[x][y];
+            k++;
+         }
 
-                y++;
+         y++;
 
-                for (x = size - 1; x >= 0; x--) {
-                    array[k] = matrix[x][y];
-                    k++;
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //do nothing
-        }
+         if (y == matrix.length) break;
 
-        System.out.println(Arrays.toString(array));
-    }
+         for (x = size - 1; x >= 0; x--) {
+            array[k] = matrix[x][y];
+            k++;
+         }
+      }
+
+      System.out.println(Arrays.toString(array));
+   }
 }
