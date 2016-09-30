8c770f2c2821340c8bfa9d5119a8a84e25134eb1
AnnaChepiga <anna.chepiga@list.ru>
2016-09-30 15:51:12 +0300

    Converting string edited

--- src/main/java/case_study_2/StringConvert.java
+++ src/main/java/case_study_2/StringConvert.java
@@ -4,38 +4,52 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StringConvert {
-    private static final Map<Character, Integer> CONVERTER = new HashMap<>();
+   private static final Map<Character, Integer> CONVERTER = new HashMap<>();
 
-    static {
-        int i; char c;
-        for (i = 0, c = 'a'; i <=9 && c <= 'j'; i++, c++) {
-            CONVERTER.put(c, i);
-        }
-    }
+   static {
+      int i;
+      char c;
+      for (i = 0, c = 'a'; i <= 9 && c <= 'j'; i++, c++) {
+         CONVERTER.put(c, i);
+      }
+   }
 
-    public static void main(String[] args) {
-        String input = "ddjb";
+   public static void main(String[] args) {
+      String input = "dD jb";
 
-        int convertedInput = convertToNumbers(input);
+      try {
+         int convertedInput = convertToNumbers(input);
+         System.out.println("string \"" + input + "\" is " + convertedInput + " in numbers");
+      } catch (NullPointerException e) {
+         System.out.println(e.getMessage());
+      }
 
-        System.out.println("string \"" + input + "\" is "  + convertedInput + " in numbers");
+      /*int convert = convertToNumbers(input);
+      if (convert != -1) {
+         System.out.println("string \"" + input + "\" is " + convert + " in numbers");
+      } else {
+         System.out.println("error: string contains unsupported symbols");
+      }*/
+   }
 
-    }
+   private static int convertToNumbers(String input) {
+      char[] inputInChars = input.toLowerCase().toCharArray();
+      String inputInNumbers = "";
+      StringBuilder builder = new StringBuilder();
 
-    private static int convertToNumbers(String input) {
-        char[] inputInChars = input.toCharArray();
-        String inputInNumbers = "";
-        StringBuilder builder = new StringBuilder();
+      for (char index : inputInChars) {
+         int number;
 
-        for (char index : inputInChars) {
-            int number = CONVERTER.get(index);
+         if (CONVERTER.containsKey(index)) {
+            number = CONVERTER.get(index);
             String s = String.valueOf(number);
-
             builder.append(s);
-
             inputInNumbers = builder.toString();
-        }
-
-        return Integer.parseInt(inputInNumbers);
-    }
+         } else {
+            //return -1;
+            throw new NullPointerException("error: string contains unsupported symbols");
+         }
+      }
+      return Integer.parseInt(inputInNumbers);
+   }
 }
